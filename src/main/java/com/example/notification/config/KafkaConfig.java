ee6401391ae9103dcc8805ca0fd1512f607cc843
Himanshu <h_himanshu2007@yahoo.com>
2025-05-24 00:06:06 +0530

    kafka external config

--- src/main/java/com/example/notification/config/KafkaConfig.java
+++ src/main/java/com/example/notification/config/KafkaConfig.java
@@ -1,13 +1,15 @@
 package com.example.notification.config;
 
 import com.example.events.NotificationEvent;
+import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -18,16 +20,19 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Autowired
+    private Environment environment;
+
     @Bean
     public ConsumerFactory<String, NotificationEvent> consumerFactory() {
         JsonDeserializer<NotificationEvent> deserializer = new JsonDeserializer<>(NotificationEvent.class);
         deserializer.addTrustedPackages("com.example.events"); // Restrict to your package in production
 
         Map<String, Object> consumerConfigs = new HashMap<>();
-        consumerConfigs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        consumerConfigs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
         consumerConfigs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         consumerConfigs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        consumerConfigs.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-service-group");
+        consumerConfigs.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
         consumerConfigs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         return new DefaultKafkaConsumerFactory<>(consumerConfigs, new StringDeserializer(), deserializer);
